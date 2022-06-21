361545a55bc459d9d9361f7b58bc07388bbac0d4
Muhammad Gilbert <gilbertariel13@gmail.com>
2022-06-21 13:38:52 +0700

    adding method PUT and DELETE

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 //import Hapi from '@hapi/hapi';
 const Hapi = require ('@hapi/hapi');
 //import * as routes from './routes.js';
-const routes = require ('./routes.js')
-const handler = require('./handler')
+// const routes = require ('./routes.js')
+// const handler = require('./handler')
 // const notes = require('./notes')
 let notes = [];
 const {nanoid} = require('nanoid')
@@ -69,7 +69,65 @@ const init = async () => {
                     data: {
                         notes,
                     }
-        })}
+        })},
+        {
+            method: 'PUT',
+            path: '/notes/{id}',
+            handler: (req, h) => {
+                const {id} = req.params;
+
+                const {title, tags, body} = req.payload;
+                const updateAt = new Date().toISOString();
+
+                const index = notes.findIndex((note) => note.id === id);
+
+                if (index !== -1){
+                    notes[index] = {
+                        ...notes[index],
+                        title,
+                        tags,
+                        body,
+                        updateAt,
+                    };
+                    const response = h.response({
+                        status: 'success',
+                        message: 'Catatan berhasil diperbarui'
+                    });
+                    response.code(200);
+                    return response;
+                }
+
+                const response = h.response({
+                    status: 'fail',
+                    message: 'Gagal memperbarui catatan, Id tidak ditemukan'
+                });
+                response.code(404);
+                return response;
+            }
+        },
+        {
+            method: 'DELETE',
+            path: '/notes/{id}',
+            handler: (req, h) => {
+                const {id} = req.params;
+
+                const index = notes.findIndex((note) => note.id === id);
+
+                if(index !== -1){
+                    notes.splice(index, 1);
+                    const response = h.response({
+                        status: 'success',
+                        message: 'Catatan berhasil dihapus',
+                    });
+                    response.code(200);
+                    return response;
+                }
+                const response = h.response({
+                    status: 'fail',
+                    message: 'Catatan gagal dihapus, Id tidak ditemukan',
+                });
+            }
+        }
     ]);
 
     await server.start();
